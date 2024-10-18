a396f4fd80e37ce230e0c120fdf9ea56cc759fbc
dinoxido <130578197+dinoxido@users.noreply.github.com>
2024-10-18 08:34:05 -0300

    bannaaaa (n me pergunte o que é isso, é lição)

---
+++ bannaa/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+console.dir(http)
+
+const arr = [1, 2, 6, 5, 9, 10]
+
+console.table(arr)
+console.dir(arr)
+console.error("ERROR!")
+console.warn("WARNING!")
+console.info(arr)
+console.time()
